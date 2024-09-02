fe3f27b15fc1df1d5266e7f65b44cbce2e53d7d6
ubeyid oumer <ubeyidah@gmail.com>
2024-09-02 20:02:35 +0300

    create subject comment schema

---
+++ api/models/subjectComment.model.js
@@ -0,0 +1,38 @@
+import { required, string } from "joi";
+import mongoose from "mongoose";
+const repliesSchema = new mongoose.Schema({
+  userId: mongoose.Types.ObjectId,
+  replie: String,
+});
+
+const subjectCommentSchema = new mongoose.Schema(
+  {
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    likes: {
+      type: [],
+      default: [],
+    },
+    replies: {
+      type: [repliesSchema],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const SubjectComment = mongoose.model("SubjectComment", subjectCommentSchema);
+export default SubjectComment;
