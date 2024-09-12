11802be06033a3dee8d14e362a2edb2d25d7b9ac
ubeyid oumer <ubeyidah@gmail.com>
2024-09-12 16:33:39 +0300

    create blog comment model

---
+++ api/models/blogComment.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const repliesSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  replie: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const blogComment = new mongoose.Schema(
+  {
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    blogId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+      required: true,
+    },
+    likes: {
+      type: [],
+      default: [],
+    },
+    replies: {
+      type: [repliesSchema],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const BlogComment = mongoose.model("SubjectComment", blogComment);
+export default BlogComment;
