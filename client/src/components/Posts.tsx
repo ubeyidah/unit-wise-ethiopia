44d43a506a319e760673562781b84f092cf1d4eb
ubeyid oumer <ubeyidah@gmail.com>
2024-09-14 10:57:35 +0300

    add infinite scroll for profile blogs

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -3,32 +3,65 @@ import {
   UserBlogInfoType,
   UserBlogsType,
 } from "@/apis/user/user.api";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Await, defer, LoaderFunction, useLoaderData } from "react-router-dom";
 import PostCard from "./PostCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { FaSpinner } from "react-icons/fa";
 
 type LoaderType = {
   data: UserBlogInfoType;
+  username: string;
 };
 export const loader: LoaderFunction = ({ params }) => {
   const { username } = params;
   const userBlogPromise = getUserBlogs(username as string);
-  return defer({ data: userBlogPromise });
+  return defer({ data: userBlogPromise, username });
 };
 
 const Posts = () => {
-  const { data } = useLoaderData() as LoaderType;
+  const { data, username } = useLoaderData() as LoaderType;
   return (
     <Suspense fallback={<h3>Loading ... .. .</h3>}>
       <Await resolve={data}>
         {(blogsInfo: UserBlogInfoType) => {
-          console.log(blogsInfo);
+          const [blogs, setBlogs] = useState(blogsInfo.blogs);
+          const [page, setPage] = useState(2);
+          const [hasMore, setHasMore] = useState(true);
+          useEffect(() => {
+            setBlogs(blogsInfo.blogs);
+          }, [blogsInfo]);
+          const fetchMoreBlog = async () => {
+            try {
+              if (!blogs) return;
+              const data = await getUserBlogs(username, page);
+              setBlogs((prevBlogs) => [
+                ...(prevBlogs as UserBlogsType[]),
+                ...data.blogs,
+              ]);
+              setPage(data.currentPage + 1);
+              setHasMore(data.currentPage < data.totalPages);
+            } catch (error) {
+              throw new Error("unable to fetch blogs");
+            }
+          };
           return (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center gap-6 py-8 gap-y-11">
-              {blogsInfo.blogs.map((blog) => (
-                <PostCard key={blog._id} {...blog} />
-              ))}
-            </div>
+            <InfiniteScroll
+              dataLength={blogs?.length || 0}
+              next={fetchMoreBlog}
+              hasMore={!!blogs && hasMore}
+              loader={
+                <div className="flex items-center justify-center my-5 gap-3">
+                  <FaSpinner className="animate-spin size-6 opacity-80" />
+                </div>
+              }
+            >
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center gap-6 py-8 gap-y-11">
+                {blogs.map((blog) => (
+                  <PostCard key={blog._id} {...blog} />
+                ))}
+              </div>
+            </InfiniteScroll>
           );
         }}
       </Await>
