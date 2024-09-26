e3b6c2aca525bc97ad37dc12678076f0a9ef5806
ubeyid oumer <ubeyidah@gmail.com>
2024-09-26 22:40:29 +0300

    fix type error import env

--- client/src/config/firebase.ts
+++ client/src/config/firebase.ts
@@ -1,20 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-// interface ImportMetaEnv {
-//   readonly VITE_API_KEY: string;
-//   readonly VITE_AUTH_DOMAIN: string;
-//   readonly VITE_PROJECT_ID: string;
-//   readonly VITE_STORAGE_BUCKET: string;
-//   readonly VITE_MESSAGING_SENDER_ID: string;
-//   readonly VITE_APP_ID: string;
-//   // Add other environment variables here...
-// }
-
-// interface ImportMeta {
-//   readonly env: ImportMetaEnv;
-// }
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY!,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN!,
